10923d112e06a2f6012fda5aff4654724168c1d0
JoellAndres <joeltuma278@gmail.com>
2020-10-24 18:35:49 -0300

    Primer commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public float parallaxSpeed = 0.02f;
+    public RawImage background;
+    public RawImage platform;
+    public GameObject uiIdle;
+
+    public enum GameState {Idle, Playing};
+    public GameState gameState = GameState.Idle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (gameState == GameState.Idle && (Input.GetKeyDown("up") || Input.GetMouseButtonDown(0)))
+        {
+            gameState = GameState.Playing;
+            uiIdle.SetActive(false);
+        }
+        else if (gameState == GameState.Playing)
+        {
+            Parallax();
+        }
+    }
+
+    void Parallax()
+    {
+        float finalSped = parallaxSpeed * Time.deltaTime;
+        background.uvRect = new Rect(background.uvRect.x + finalSped, 0.0f, 1.0f, 1.0f);
+        platform.uvRect = new Rect(platform.uvRect.x + finalSped * 4, 0.0f, 1.0f, 1.0f);
+    }
+}
